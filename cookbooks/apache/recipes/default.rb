dfc21123ba7ec879027e093f095102079286ac27
Sebastien Dejean <sdejean2298@gmail.com>
2015-02-19 16:52:42 -0800

    moving package name to a variable

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,8 +4,10 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-package 'httpd'
-package 'httpd-tools'
+package_name = 'httpd'
+
+package "#{package_name}"
+package "#{package_name}-tools"
 
 service 'httpd' do
   action [:enable, :start]
