4944c346461ecbfeeb2860b72055091adba195be
Sebastien Dejean <sdejean2298@gmail.com>
2015-02-19 16:17:29 -0800

    Check if apache is running

--- cookbooks/apache/test/integration/default/serverspec/default_spec.rb
+++ cookbooks/apache/test/integration/default/serverspec/default_spec.rb
@@ -5,8 +5,12 @@ describe 'apache::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
   
-  it 'installed' do
+  it 'is installed' do
     expect(package('httpd')).to be_installed
   end
 
+  if 'is running' do
+    expect(service('httpd')).to be_enabled
+    expect(service('httpd')).to be_running
+  end
 end
